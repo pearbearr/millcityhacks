06e420872ac86e6f19f5b97a4ff629c9fa61312f
DIodide <68827140+DIodide@users.noreply.github.com>
2024-07-28 01:28:04 +0000

    Add countdown and hero page

---
+++ components/Countdown.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export default function Countdown(props: { targetDate: string }) {
+  const countDownDate = new Date(props.targetDate).getTime();
+  let timeDelta = countDownDate - new Date().getTime();
+  const [countDown, setCountDown] = useState(timeDelta);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      let timeDelta = countDownDate - new Date().getTime();
+      setCountDown(timeDelta);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [countDown]);
+
+  return (
+    <div className="flex items-center justify-center m-[10%] p-5 bg-black text-white text-[4em] font-bold rounded-xl">
+      <div className="">
+        {getReturnValues(countDown)
+          .map((x) => String(x).padStart(2, "0"))
+          .join(":")}
+      </div>
+    </div>
+  );
+}
+
+function getReturnValues(countDown: number) {
+  // calculate time left
+  const days = Math.floor(countDown / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+  );
+  const minutes = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((countDown % (1000 * 60)) / 1000);
+
+  return [days, hours, minutes, seconds];
+}
---
+++ components/HomeTypewriter.tsx
@@ -0,0 +1,49 @@
+"use client";
+import { TypewriterEffect } from "./ui/typewriter-effect";
+
+export function HeroTaglineWriter() {
+  const words = [
+    {
+      text: "A",
+    },
+    {
+      text: "hackathon",
+    },
+    {
+      text: "for",
+    },
+    {
+      text: "high",
+    },
+    {
+      text: "schoolers",
+    },
+    {
+      text: "by",
+    },
+    {
+      text: "high",
+      className: "text-blue-500 dark:text-blue-500",
+    },
+    {
+      text: "schoolers.",
+      className: "text-blue-500 dark:text-blue-500",
+    },
+  ];
+  return (
+    <div className="flex flex-col items-center justify-center h-[40rem] ">
+      <p className="text-neutral-600 dark:text-neutral-200 text-base  mb-10">
+        The Programming Initiative presents...
+      </p>
+      <TypewriterEffect words={words} />
+      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 mt-10">
+        <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
+          Get Notified
+        </button>
+        <button className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
+          Register
+        </button>
+      </div>
+    </div>
+  );
+}
