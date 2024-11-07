8ff84d73c83ea9ef890553ca9bd6c14bfafbc67f
Pear <perrypho25@gmail.com>
2024-11-06 23:01:01 -0500

    Third Try

--- components/SponsorsCarousel.tsx
+++ components/SponsorsCarousel.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 
 const sponsors = [
-  '/images/pngs/xyz-logo-white.png',
+  'public/images/pngs/xyz-logo-white.png',
   '/images/sponsor2.png',
     '/images/sponsor3.png',
   '/images/sponsor1.png',
